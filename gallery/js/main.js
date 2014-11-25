1f2b547d63b83db90cbd35ad834cb6706067d7ee
jackpopp <jack.popp@hotmail.com>
2014-11-25 09:40:04 +0000

    improved javascript.

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -1,60 +1,73 @@
-$holder = null
-stream = null
-entryId = null
+(function(){
 
-function updateImage(val, response)
-{
-	$img = $('.js-gallery-image').first().clone()
+	$holder = null
+	stream = null
+	entryId = null
 
-	// clear caption
-	$img.find('.js-caption-img').val('')
-
-	//update ids 
-	id = response['id']
-
-	// set image
-	$img.find('img').first().attr('src', response['path'])
-
-	$('.js-gallery-holder').append($img)
-};
-
-function deleteImage(element)
-{
-	if (confirm('Are you sure you wish to delete this image?'))
+	function updateImage(val, response)
 	{
-		index = $('.js-image-remove').index(element)
+		$img = $('.js-gallery-image').first().clone();
 
-		image = $($('.js-gallery-image')[index])
+		// clear caption
+		$img.find('.js-caption-img').val('');
 
-		id = image.data('id')
+		//update ids 
+		id = response['id'];
+		$img.attr('data-id', id)
 
-		request = $.post("streams_core/public_ajax/field/gallery/delete_image", {id : id})
+		// set image
+		$img.find('img').first().attr('src', response['path']);
 
-		request.done(function(){
-			image.remove()
-		});
+		$img.removeClass('hidden');
+
+		$('.js-gallery-holder').append($img);
+	};
+
+	function deleteImage(element)
+	{
+		if (confirm('Are you sure you wish to delete this image?'))
+		{
+			index = $('.js-image-remove').index(element)
+
+			image = $($('.js-gallery-image')[index])
+
+			id = image.data('id')
+
+			request = $.post("streams_core/public_ajax/field/gallery/delete_image", {id : id})
+
+			request.done(function(){
+				image.remove()
+			});
+		}
+	};
+
+	function uploadError()
+	{
+		alert('There was an error uploading the image');
 	}
-}
 
-$(function(){
+	$(function(){
 
-	$holder = $('.js-gallery-holder')
-	stream = $holder.data('stream')
-	entryId = $holder.data('entry-id')
+		$holder = $('.js-gallery-holder')
+		stream = $holder.data('stream')
+		entryId = $holder.data('entry-id')
 
 
-	$(".dropzone-js").dropzone({ 
-		paramName: 'userfile',
-		url: "streams_core/public_ajax/field/gallery/upload_image",
-		previewsContainer: false,
-		success: updateImage,
-		maxFilesize: 2,
-		acceptedFiles: 'image/jpeg,image/png,image/jpg',
-		params: { stream: stream, entry_id: entryId }
+		$(".dropzone-js").dropzone({ 
+			paramName: 'userfile',
+			url: "streams_core/public_ajax/field/gallery/upload_image",
+			previewsContainer: false,
+			success: updateImage,
+			maxFilesize: 2,
+			acceptedFiles: 'image/jpeg,image/png,image/jpg',
+			params: { stream: stream, entry_id: entryId },
+			error: uploadError
+		});
+
+		$(document).on('click', '.js-image-remove', function(){
+			deleteImage(this)
+		})
+
 	});
 
-	$(document).on('click', '.js-image-remove', function(){
-		deleteImage(this)
-	})
-
-});
+})();
